ebb62505d64905dcd68c4c9407544c08311d333c
Olivia Davis <olivia.davis593@gmail.com>
2020-06-13 15:46:23 -0500

    image not populating

--- src/components/BrewItem.js
+++ src/components/BrewItem.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 
 class BrewItem extends Component {
     render() {
-        const { name, id } = this.props; 
+        const { image, name, id } = this.props; 
 
         return(
             <div className='brew-item'>
+                <div className='brew-image'>
+                    {image}
+                </div>
                 <div>
                   <Link to={`/brews/${id}`}>
                     <p className='gbt-item__brew_name'>
