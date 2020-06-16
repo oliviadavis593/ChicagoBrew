15cd229c6d8f98f83c7d98979a2e130320a96388
Olivia Davis <olivia.davis593@gmail.com>
2020-06-15 21:19:45 -0500

    more data added

--- src/components/store.js
+++ src/components/store.js
@@ -35,6 +35,330 @@ export default {
             "phone_number": "(219) 922-3565",
             "details": "Happening craft brewery & pub with an edgy vibe & inventive small plates, plus facility tours.",
             "website": "https://shop.3floyds.com/collections/all"
+        },
+        {
+            "id": 5, 
+            "name": "93 Octane Brewery",
+            "image": require('../img/93octane.png'),
+            "address": "1825 Lincoln St, St. Charles, IL 60174",
+            "phone_number": "(630) 549-0332",
+            "details": "At 93 Octane our goal is to produce great beer for everyone to enjoy. We have a passion for Craft Beer and we constantly strive to find the best Local Breweries to carry at our PRIDE Locations. The name of our Brewery is a play towards our Roots. We have been running Gasoline Stations for over 40 Years as a Second Generation Family Business. ",
+            "website": "http://www.93octanebrewery.com/"
+        },
+        {
+            "id": 6, 
+            "name": "Malt Bros. at 95ate5",
+            "image": require('../img/maltbros.png'),
+            "address": "9430, 9585 N Industrial Dr, St John, IN 46373",
+            "phone_number": "(219) 365-6506",
+            "details": "American comfort fare, breakfast specials & beers are featured at this comfy spot with a warm vibe.",
+            "website": "https://www.maltbros.com/"
+        },
+        {
+            "id": 7, 
+            "name": "Adams Street Brewery",
+            "image": require('../img/adamstreet.png'),
+            "address": "17 W Adams St, Chicago, IL 60603",
+            "phone_number": "(312) 427-5820",
+            "details": "In 1870, Herman Berghoff arrived in the United States from Germany at the age of 17. Over the years, he proved himself to be an excellent entrepreneur, growing the popularity of the brewery, fighting Prohibition by brewing “near-beer” and Bergo soda pops (root beer), and turning his rejection for participation in Chicago’s World Fair into an opportunity by setting up a beer station just outside the Fair. Today, his name is most well-known for the 120-year-old historic establishment in Chicago, The Berghoff Restaurant.",
+            "website": "https://www.adamsstreetbrewery.com/"
+        },
+        {
+            "id": 8, 
+            "name": "Afterthought Brewing Company",
+            "image": require('../img/afterthought.png'),
+            "address": "844 N Ridge Ave, Lombard, IL 60148",
+            "phone_number": "n/a",
+            "details": "We are a small, family-run, saison-focused brewery in Lombard, Illinois brewing 1.5-barrel batches. Saison is a style that was born in the Belgian and French countryside, traditionally consumed by field laborers. We believe saisons should be dry, relatively low in alcohol, highly carbonated, and thirst quenching.",
+            "website": "https://afterthoughtbrewing.com/"
+        },
+        {
+            "id": 9, 
+            "name": "Alarmist Brewing & Taproom",
+            "image": require('../img/alarmist.png'),
+            "address": "4055 West Peterson Avenue, Suite Rear, Chicago, IL 60646",
+            "phone_number": "(773) 681-0877",
+            "details": "Tasting room of a microbrewery with plentiful seating & a bar in a funky warehouse setting.",
+            "website": "https://alarmistbrewing.com/"
+        },
+        {
+            "id": 10, 
+            "name": "Aleman Brewing Company",
+            "image": require('../img/aleman.png'),
+            "address": "3304 N Knox Ave, Chicago, IL 60641",
+            "phone_number": "(812) 340-4198",
+            "details": "MAN. WOMAN. HUMAN. ALEMAN.",
+            "website": "https://www.alemanchicago.com/"
+        },
+        {
+            "id": 11, 
+            "name": "All Rise Brewing Company",
+            "image": require('../img/allrise.png'),
+            "address": "235 N Ashland Ave, Chicago, IL 60607",
+            "phone_number": "(312) 226-6300",
+            "details": "After nine years of support from the greatest patrons and musicians from around the globe, we built All Rise Brewing Co alongside Cobra Lounge in an effort to bring even more of ourselves and community to the public.",
+            "website": "https://www.allrisebrewing.com/"
+        },
+        {
+            "id": 12, 
+            "name": "Alter Brewing Company ",
+            "image": require('../img/alter.png'),
+            "address": "2300 Wisconsin Ave Ste 213, Downers Grove, IL 60515",
+            "phone_number": "(630) 541-9558",
+            "details": "We’re not brewing just for beer judges.* We’re making creative, delicious, drinkable beers for everyone. We’re for any time you get together. It’s beer for anywhere. See what’s brewing.",
+            "website": "https://www.alterbrewing.com/"
+        },
+        {
+            "id": 13, 
+            "name": "Alulu Brewery",
+            "image": require('../img/alulu.png'),
+            "address": "2011 S Laflin St, Chicago, IL 60608",
+            "phone_number": "2011 S Laflin St, Chicago, IL 60608",
+            "details": "Warehouse-style spot for pairing house-brewed beers, craft cocktails & sodas with creative pub fare.",
+            "website": "http://www.alulubrew.com/"
+        },
+        {
+            "id": 14, 
+            "name": "Around the Bend Beer Company",
+            "image": require('../img/atb.png'),
+            "address": "417 N Ashland Ave, Chicago, IL 60622",
+            "phone_number": "n/a",
+            "details": "We're not renegades. We're not rebelling against anything. We think that the revolution is over; the war has been won.",
+            "website": "http://atbbeerco.com/"
+        },
+        {
+            "id": 15, 
+            "name": "Arrowhead Ales Brewing Company",
+            "image": require('../img/arrowhead.png'),
+            "address": "2101 Calistoga Dr, New Lenox, IL 60451",
+            "phone_number": "(815) 717-6068",
+            "details": "All the beers at this spacious brewhouse venue are brewed in-house & served with classic bites.",
+            "website": "https://arrowheadales.com/"
+        },
+        {
+            "id": 16, 
+            "name": "Art History Brewing",
+            "image": require('../img/arthistory.png'),
+            "address": "649 W State St, Geneva, IL 60134",
+            "phone_number": "(630) 345-6274",
+            "details": "The idea of Art History has been brewing in the back of Tom Rau's mind since the early 1980s when he began home brewing and trying traditional styles of beer at the old Amsterdam Brew Pub in Toronto, Ontario. From those early days, Tom was hooked.",
+            "website": "https://arthistorybrewing.com/"
+        },
+        {
+            "id": 17, 
+            "name": "Ballast Point Brewing Chicago",
+            "image": require('../img/ballast.png'),
+            "address": "212 N Green St, Chicago, IL 60607",
+            "phone_number": "(312) 243-9570",
+            "details": "We started out in 1996 as a small group of San Diego home brewers who simply wanted to make a better beer. In our exploration, we became obsessed with ingredients—tinkering, testing and tasting to find the perfect balance of taste and aroma. That adventurous spirit leads to award-winning beers in classic and unique styles while challenging our own tastes and expanding yours.",
+            "website": "https://ballastpoint.com/"
+        },
+        {
+            "id": 18, 
+            "name": "Band of Bohemia",
+            "image": require('../img/bob.png'),
+            "address": "4710 N Ravenswood Ave, Chicago, IL 60640",
+            "phone_number": "(773) 271-4710",
+            "details": "Funky-chic brewpub infusing culinary flavors into house beers & pairing them with global plates.",
+            "website": "https://www.bandofbohemia.com/"
+        },
+        {
+            "id": 19, 
+            "name": "Begyle Brewing Company",
+            "image": require('../img/beygle.png'),
+            "address": "1800 W Cuyler Ave, Chicago, IL 60613",
+            "phone_number": " (773) 661-6963",
+            "details": "Straightforward microbrewery & retailer teaming its craft beers with tours & more.",
+            "website": "http://www.begylebrewing.com/"
+        },
+        {
+            "id": 20, 
+            "name": "BiXi Beer",
+            "image": require('../img/bixi.png'),
+            "address": "2515 N Milwaukee Ave, Chicago, IL 60647",
+            "phone_number": "(773) 904-7368",
+            "details": "Trendy, modern brewpub featuring craft brews, cocktails & Asian-inspired eats, plus a rooftop patio.",
+            "website": "https://www.bixi.beer/"
+        },
+        {
+            "id": 21, 
+            "name": "Black & Gray Brewing Co.",
+            "image": require('../img/blackandgray.png'),
+            "address": "311 Barrington Ave Unit B, East Dundee, IL 60118",
+            "phone_number": "(224) 484-8200",
+            "details": "Black & Gray Brewing is the labor of love of Chris and Teresa, a couple of average, ordinary kids, from a blue collar neighborhood, who grew up down the street from each other, got married, and moved back to that same great blue collar neighborhood to raise their kids in a loving community filled with cops, firefighters, roofers, electricians, teachers, and carpenters.  You know, blue collar folk!",
+            "website": "http://www.blackandgraybrewing.com/"
+        },
+        {
+            "id": 22, 
+            "name": "Black Horizon Brewing Company",
+            "image": require('../img/blackhorizon.png'),
+            "address": "7560 S Quincy St, Willowbrook, IL 60527",
+            "phone_number": "(630) 413-4964",
+            "details": "We are a small micro brewery with a taproom that turns out award winning barrel aged beer , rotating styles and varieties. Just three guys who brought the vision of opening a small community brewery to reality. We do not make batches of 1000 gallons at a time so things will change frequently, so come back often to try something new and exciting!",
+            "website": "https://www.blackhorizonbrewing.com/home"
+        },
+        {
+            "id": 23, 
+            "name": "Black Lung Brewing Company",
+            "image": require('../img/blacklung.png'),
+            "address": "3233 W. Monroe Ave., Waukegan, IL 60085",
+            "phone_number": "(847)340-3320",
+            "details": "Black Lung Brewing Company is a brewery grounded not just in family, but their path and their story.  Beer for hard working folks (like Big Bill) and for those who enjoy it.  Beer, grounded in tradition, inspired by enthusiasm! ",
+            "website": "https://blacklungbrewing.com/"
+        },
+        {
+            "id": 24, 
+            "name": "Blue Island Beer Company",
+            "image": require('../img/blueisland.png'),
+            "address": "13357 Old Western Ave, Blue Island, IL 60406",
+            "phone_number": "(708) 954-8085",
+            "details": "Our inspiration comes from a foundation in classic styles. From an understanding of what makes each style great, we are able to diverge from the basics in specialty beers that compliment the style, history and flavors in tasty ways.",
+            "website": "https://blueislandbeerco.com/"
+        },
+        {
+            "id": 25, 
+            "name": "Blue Nose Brewery",
+            "image": require('../img/bluenose.png'),
+            "address": "6119 East Ave, Hodgkins, IL 60525",
+            "phone_number": "(708) 905-5198",
+            "details": "Sports, Arcade Games, Food Trucks & more...",
+            "website": "https://bluenosebrewery.com/"
+        },
+        {
+            "id": 26, 
+            "name": "Bold Dog Beer Co.",
+            "image": require('../img/bolddog.png'),
+            "address": "417 N Ashland Ave, Chicago, IL 60622",
+            "phone_number": "",
+            "details": "Bold Dog Beer Company focuses on Belgian and American style brews and will expand into German and other experimental styles at their new home at the District Brew Yards.",
+            "website": "https://districtbrewyards.com/bold-dog-beer-co/"
+        },
+        {
+            "id": 27, 
+            "name": "Bosacki's Home Brew",
+            "image": require('../img/bosackis.png'),
+            "address": "610 E Hawley St, Mundelein, IL 60060",
+            "phone_number": "(224) 778-5400",
+            "details": "Bosacki's Brewery is a local microbrewery and taproom producing traditional and historic style beers, created with concept of tasting beer and taking it home to enjoy with family and friends. Whether it's for the football game, family gathering, or weekend bonfire it is meant to be shared and enjoyed fresh. ",
+            "website": "https://www.bosackishomebrew.com/home"
+        },
+        {
+            "id": 28, 
+            "name": "Brothership Brewing",
+            "image": require('../img/brothership.png'),
+            "address": "18781 S 90th Ave, Mokena, IL 60448",
+            "phone_number": "(708) 995-7014",
+            "details": "Brothership Brewing is dedicated to making quality beer while providing a comfortable and friendly atmosphere in our new brewery/taproom. We stay true to our craft by using only the finest hops, water, grains and yeast available. As beer enthusiasts, we find true joy in sharing the results of our passion with the craft beer community. Growth through curiosity. Innovation through exploration.",
+            "website": "https://www.brothershipbrewing.com/"
+        },
+        {
+            "id": 29, 
+            "name": "Brother Chimp Brewing",
+            "image": require('../img/brotherchimp.png'),
+            "address": "1059 W Orchard Rd, North Aurora, IL 60542",
+            "phone_number": "(630) 800-1724",
+            "details": "Brother Chimp Brewing aspires to be your first choice for handcrafted classic styles of beer. Above all, we believe beer should be fun!",
+            "website": "https://brotherchimpbrewing.com/"
+        },
+        {
+            "id": 30, 
+            "name": "BuckleDown Brewing",
+            "image": require('../img/buckledown.png'),
+            "address": "8700 47th St, Lyons, IL 60534",
+            "phone_number": "(708) 777-1842",
+            "details": "An independent craft brewery located just outside of Chicago, IL.",
+            "website": "https://buckledownbrewing.com/"
+        },
+        {
+            "id": 31, 
+            "name": "Bulldog Brewing Co.",
+            "image": require('../img/bulldog.png'),
+            "address": "1409 119th St, Whiting, IN 46394",
+            "phone_number": "(219) 655-5284",
+            "details": "Low-key haunt offering elevated pub grub, microbrews & cocktails in comfy brick-walled digs.",
+            "website": "http://bulldogbrewingco.com/"
+        },
+        {
+            "id": 32, 
+            "name": "Burning Bush Brewing",
+            "image": require('../img/burningbush.png'),
+            "address": "4014 N Rockwell St, Chicago, IL 60618",
+            "phone_number": "(847) 858-0641",
+            "details": "Hip microbrewery hangout pouring house beers with tap wines, snacks & occasional food trucks.",
+            "website": "https://www.burningbushbrewery.com/"
+        },
+        {
+            "id": 33, 
+            "name": "Burnt City Brewing Company",
+            "image": require('../img/burntcity.png'),
+            "address": "417 N Ashland Ave, Chicago, IL 60622",
+            "phone_number": "(773) 295-1270",
+            "details": "We are currently brewing beer in the West Town neighborhood of Chicago, inside the District Brew Yards. This brewing collective is the first of its kind in the nation, housing a brewery, beer hall and shop in one location. We’re proud to be stewards of this unique concept and the positive change it brings to the brewing community.",
+            "website": "https://burntcitybrewing.com/"
+        },
+        {
+            "id": 34, 
+            "name": "Byway Brewing Company",
+            "image": require('../img/byway.png'),
+            "address": "2825 Carlson Dr, Hammond, IN 46323",
+            "phone_number": "(219) 844-5468",
+            "details": "While a career in commodities remained an exciting one, Dave became passionate enough about craft beer to devote the 4th quarter of his life to building and running a microbrewery.  In the summer of 2014, after an exhaustive vetting of Northwest Indiana towns and buildings, good friends Branko Sajn (Munster) and Tom Duszynski (Chicago) added their expertise in industrial instrumentation, an upscale restaurant and accounting to the mix and a business plan was formed.  Family members Phil Sajn (Taproom General Manager) and Angie Toth (Marketing & Sales Manager) run the day-to-day operations.",
+            "website": "https://www.bywaybrewing.beer/"
+        },
+        {
+            "id": 35, 
+            "name": "Cary Ale House & Brewing Company",
+            "image": require('../img/cary.png'),
+            "address": "208 W Main St, Cary, IL 60013",
+            "phone_number": "(847) 639-7244",
+            "details": "Hip neighborhood haunt serving locally sourced, clever pub fare & craft beers in a historical space.",
+            "website": "http://caryalehousebrewing.com/"
+        },
+        {
+            "id": 36, 
+            "name": "Corridor Brewery & Provisions",
+            "image": require('../img/corridor.png'),
+            "address": "3446 N Southport Ave, Chicago, IL 60657",
+            "phone_number": "(773) 270-4272",
+            "details": "Corridor Brewery & Provisions is a craft brewery and restaurant in Chicago’s West Lakeview neighborhood, located in the center of the Southport Corridor.",
+            "website": "https://www.corridorchicago.com/"
+        },
+        {
+            "id": 37, 
+            "name": "Crown Brewing",
+            "image": require('../img/crown.png'),
+            "address": "4058, 211 S East St, Crown Point, IN 46307",
+            "phone_number": "219) 663-4545",
+            "details": "This relaxed brewpub features a selection of craft beers, plus events & brewery tours.",
+            "website": "https://www.crownbrewing.com/"
+        },
+        {
+            "id": 38, 
+            "name": "Cruz Blanca Cervecería",
+            "image": require('../img/cruzblanca.png'),
+            "address": "904 W Randolph St, Chicago, IL 60607",
+            "phone_number": "(312) 733-1975",
+            "details": "Hip brewery & Mexican-influenced pub from Rick Bayless with a visible production area & a taco menu.",
+            "website": "https://www.rickbayless.com/restaurants/cerveceria-cruz-blanca/"
+        },
+        {
+            "id": 39, 
+            "name": "Dovetail Brewery",
+            "image": require('../img/dovetail.png'),
+            "address": "1800 W Belle Plaine Ave, Chicago, IL 60613",
+            "phone_number": "(773) 683-1414",
+            "details": "Chill taproom for sampling house-brewed beers based on classic European styles in a vast space.",
+            "website": "http://dovetailbrewery.com/"
+        },
+        {
+            "id": 40, 
+            "name": "DryHop Brewers",
+            "image": require('../img/dryhop.png'),
+            "address": "3155 N Broadway, Chicago, IL 60657",
+            "phone_number": "(773) 857-3155",
+            "details": "European-inspired gastropub plates & seasonal house brews in a rustic, laid-back space.",
+            "website": "https://dryhopchicago.com/"
         }
     ]
 }
