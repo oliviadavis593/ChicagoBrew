d049245cf9dd34d9409da41f8ed0a1a3408d9868
Olivia Davis <olivia.davis593@gmail.com>
2020-06-23 10:07:38 -0500

    error handler

---
+++ src/Error.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false 
+        }
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <h2>Something seems to be wrong...Please try again later</h2>
+            )
+        }
+        return this.props.children
+    }
+}
+
+export default Error; 
