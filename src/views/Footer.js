06032e6217a0f33b89923e3b064662dd3e7813d5
Olivia Davis <olivia.davis593@gmail.com>
2020-06-23 23:14:56 -0500

    warning removed

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <footer className='footer'>
             <div className='footer__brand-container'>
                 <span role='img'>
-                    Made with ❤️ by <a href='https://oliviadavis593.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Olivia Davis</a>
+                    Made with <span role='img' aria-label='heart-emoji'>❤️</span> by <a href='https://oliviadavis593.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Olivia Davis</a>
                 </span>
             </div>
             <div className='footer__social-media'>
