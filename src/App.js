1483e98b8ed41e664165125c5323c4ae1a998c78
Olivia Davis <olivia.davis593@gmail.com>
2020-06-23 21:38:42 -0500

    test passing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppRouter from './routers/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <AppRouter />
+   <BrowserRouter>
+     <AppRouter />
+   </BrowserRouter>
   );
 }
 
